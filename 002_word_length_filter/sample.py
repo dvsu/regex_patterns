8407807a697ae48e5e0bc6ba30a22de9d7b1a41c
David S <dave.techdev@gmail.com>
2021-07-25 00:01:59 +0700

    Add 002 example

--- 002_word_length_filter/sample.py
+++ 002_word_length_filter/sample.py
@@ -2,7 +2,7 @@ import re
 
 
 def word_length_filter(text: str, min_length: int) -> list:
-    return re.findall(r"\w{"+str(length)+r",}", text)
+    return re.findall(r"\w{"+str(min_length)+r",}", text)
 
 
 print(word_length_filter(
@@ -14,3 +14,16 @@ print(word_length_filter(
 
 # Expected result
 # ['conversation', 'animated', 'Ludovic', 'somebody', 'frequently', 'surprised', 'comments', 'matters', 'reached']
+
+print(word_length_filter(
+    text="There were no Mayflowers in June; \
+        but now the Old Lady’s garden was full of blossoms and \
+        every morning Sylvia found a bouquet of them by the beech—\
+        the perfumed ivory of white narcissus, the flame of tulips, \
+        the fairy branches of bleeding-heart, the pink-and-snow of little, \
+        thorny, single, sweetbreathed early roses. The Old Lady had no fear of discovery, \
+        for the flowers that grew in her garden grew in every other Spencervale garden as well, \
+        including the Stewart garden. .", min_length=10))
+
+# Expected result
+# ['Mayflowers', 'sweetbreathed', 'Spencervale']
