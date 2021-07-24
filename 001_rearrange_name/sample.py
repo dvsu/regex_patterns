316e58dcd0ad5e94a0a64f1ee99c460db1791303
David S <dave.techdev@gmail.com>
2021-07-24 23:33:44 +0700

    Local commit

---
+++ 001_rearrange_name/sample.py
@@ -0,0 +1,55 @@
+import re
+
+
+def rearrange_name(name):
+    # Add a period '.' and space inside square bracket to capture period and space after a word
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)", name)
+    return name if not result else f"{result[2]} {result[1]}"
+
+
+# Fictional name list which consists of
+# - names in reversed order
+# - single-word names
+# - names with abbreviated middle names
+# - names with one middle name
+# - names with more than one middle name
+
+
+name_list = [
+    "Hodson, Lauren K.",
+    "Gabrielle",
+    "Watkins, Brandee",
+    "Pitts, Zachary",
+    "Swift, Serina",
+    "Blakeslee, Brant I.",
+    "Thatcher, Brandy Jermaine",
+    "Raleigh",
+    "Hurst, Carter",
+    "Hopper, Jarvis",
+    "Garnet, Evalyn Houston Fiona",
+    "Audley, Matthew",
+    "Pottinger, Merlin A. K.",
+    "Reeves, Ezra",
+    "Pearson, Osborn K."
+]
+
+# Expected result:
+
+# Lauren K. Hodson
+# Gabrielle
+# Brandee Watkins
+# Zachary Pitts
+# Serina Swift
+# Brant I. Blakeslee
+# Brandy Jermaine Thatcher
+# Raleigh
+# Carter Hurst
+# Jarvis Hopper
+# Evalyn Houston Fiona Garnet
+# Matthew Audley
+# Merlin A. K. Pottinger
+# Ezra Reeves
+# Osborn K. Pearson
+
+for name in name_list:
+    print(rearrange_name(name))
